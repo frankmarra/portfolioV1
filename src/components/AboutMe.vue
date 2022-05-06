6d6fdc353aff7ecab6093741d4af6c628d82f2fc
Frank Marra <frankrmarra@gmail.com>
2022-05-05 21:35:06 -0400

    added cycle text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,33 @@
     <div class="about-me-copy-wrapper">
       <div class="about-me-copy">
       <h3>Thank's for stopping by</h3>
-      <p>I am a software engineer with a passion for creation.  I have walked many paths, worn many hats, and the main constant throughout is the making of something from nothing.</p>
+      <p>I have walked many paths and one thing that has always remained constant is my love of creation. Whether I am writing a song, building a house, or developing an app, nothing is more satisfying than stepping back and seeing what I have helped create.		
+      </p>
+      <h4>Skills</h4>
+      <div class="skills-list">
+      <ul class="front-end-skills-list">
+        <h4>Front End</h4>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React including Redux and React-Router</li>
+        <li>Vue including Vue-Router</li>
+      </ul>
+      <ul class="back-end-skills-list">
+        <h4>Back End</h4>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>SQL</li>
+        <li>PostgreSQL</li>
+        <li>Sequelize</li>
+        <li>Python</li>
+        <li>Django</li>
+        <li>JWT Auth</li>
+        <li>Insomnia</li>
+        </ul>
+      </div>
       </div>
     </div>
   </div>
--- src/components/MenuButtons.vue
+++ src/components/MenuButtons.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="menu-buttons">
-      <div class="menu-button-wrapper" @click="navigateItem('portfolio')">
-          <button class="portfolio-button menu-button">Portfolio</button>
-      </div>
-      <div class="menu-button-wrapper" @click="navigateItem('/')">
+      <div class="menu-button-wrapper" @click="navigateItem('')">
           <button class="about-button menu-button">About Me</button>
+      </div>
+      <div class="menu-button-wrapper" @click="navigateItem('portfolio')">
+          <button class="portfolio-button menu-button">Projects</button>
       </div>
       <div class="menu-button-wrapper" @click="navigateItem('resume')">
           <button class="resume-button menu-button" name="Resume" >Resume</button>
